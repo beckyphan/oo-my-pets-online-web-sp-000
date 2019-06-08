dfa116cb93ad0a24add6136b80ff1ebcdd37d32e
Becky Phan <beckyphan@live.com>
2019-06-08 22:30:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
   end
 
   def list_pets 
-    nfish = self.pets.values_at
+    nfish = self.pets.values_at(:fishes).count
     ndogs = self.pets.count([:fishes])
     ncats = self.pets.count([:fishes])
     "I have #{nfish} fish, #{ndogs} dog(s), and #{ncats} cat(s)."
